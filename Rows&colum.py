ceaf37f42a03985ed0b42372b0782d382b21bc7f
yoummumerson <reyaansh.shoumik@gmail.com>
2026-06-09 19:13:44 +0900

    Add files via upload

---
+++ Rows&colum.py
@@ -0,0 +1,7 @@
+rows = int(input("Enter number of rows:"))
+cols = int(input("Enter number of columns:"))\
+
+for i in range(rows):
+    for j in range(cols):
+        print("*",end="")
+    print ()
